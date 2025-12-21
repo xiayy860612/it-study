0e2b1378b876f2360ba4bfede3c9135bfab2c426
Amos Xia <xiayy860612@126.com>
2025-12-21 20:54:12 +0800

    review algo

---
+++ algo-study/README.md
@@ -0,0 +1,4 @@
+# 算法学习
+
+- [算法面试通关 40 讲](https://time.geekbang.org/course/intro/100019701)
+- []()
---
+++ algo-study/src/main/java/com/s2u2m/it_study/common/priority_queue/KthLargest.java
@@ -0,0 +1,27 @@
+package com.s2u2m.it_study.common.priority_queue;
+
+import java.util.PriorityQueue;
+
+/**
+ * <a href="https://leetcode.cn/problems/kth-largest-element-in-a-stream/" />
+ */
+public class KthLargest {
+    private final PriorityQueue<Integer> pq;
+    private final int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        pq.offer(val);
+        while (pq.size() > this.k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
